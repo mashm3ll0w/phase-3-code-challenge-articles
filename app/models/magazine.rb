6b975d4bc57073ce1c713d906e7267d310fb28fd
Charles Swaleh <charlswall@live.com>
2023-03-21 19:27:28 +0300

    Core Deliverables - 2 - completed the Magazine class methods

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,11 +1,23 @@
 class Magazine
-  attr_accessor :name, :category
+  # attr_accessor :name, :category
 
-
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
+    @@all << self
+  end
 
+  def name
+    @name
+  end
+
+  def category
+    @category
+  end
+
+  def self.all
+    @@all
   end
 
 
