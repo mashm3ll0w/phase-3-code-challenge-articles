5c182b2d760a0b30a6af1b3d7cf80acee3206287
Charles Swaleh <charlswall@live.com>
2023-03-21 19:48:34 +0300

    Core Deliverables - 3 - completed the Article class methods

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,31 @@
 class Article
+    attr_reader :author, :magazine, :title
 
+    @@all = []
 
+    def initialize(author, magazine, title)
+
+        if author.is_a?(Author) && magazine.is_a?(Magazine)
+            @author = author
+            @magazine = magazine
+            @title = title
+            @@all << self
+        else
+            begin
+                raise WrongClassError
+            rescue WrongClassError => error
+                puts error.message
+            end
+        end
+    end
+
+    def self.all
+        @@all
+    end
+
+    class WrongClassError < StandardError
+        def message
+            "Check the data types of the Author and/or Magazine"
+        end
+    end
 end
